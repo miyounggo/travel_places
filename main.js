f319e3b6ac70add13a96111ce3691f57ed61e219
miyounggo <133185495+miyounggo@users.noreply.github.com>
2023-05-30 18:06:51 +0900

    script

---
+++ main.js
@@ -0,0 +1,37 @@
+document.querySelector("#btn01").addEventListener("click", () => {
+    alert("남해 독일마을 상세페이지로 이동합니다!");
+    location.href="http://남해독일마을.com" 
+});
+
+
+
+
+document.querySelector("#btn02").addEventListener("click", () => {
+    alert("삼양목장 상세페이지로 이동합니다!");
+    location.href="https://www.samyangfarm.co.kr/"
+});
+document.querySelector("#btn03").addEventListener("click", () => {
+    alert("마이산 상세페이지로 이동합니다!");
+    location.href="https://jinan.go.kr/maisan/"
+});
+document.querySelector("#btn04").addEventListener("click", () => {
+    alert("산토리니 상세페이지로 이동합니다!");
+    location.href="https://www.webcamgreece.com/webcam-santorini-oia-village.html"
+});
+document.querySelector("#btn05").addEventListener("click", () => {
+    alert("그랜드캐년상세페이지로 이동합니다!");
+    location.href="https://www.expedia.co.kr/Grand-Canyon-Visitor-Center-Grand-Canyon.d553248621562820554.Place-To-Visit"
+});
+document.querySelector("#btn06").addEventListener("click", () => {
+    alert("디즈니 월드 상세페이지로 이동합니다!");
+    location.href="https://disneyworld.disney.go.com/"
+});
+
+
+
+let closeBTN = document.querySelector("#close_modal");
+let modalBOX = document.querySelector(".modal");
+closeBTN.addEventListener("click",() => { 
+    modalBOX.style.display = "none";
+});
+    
